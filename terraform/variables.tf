8df375a94ca2f7de13c7e19416e0d28e428f6953
Noah Dimmer <noahdimmer1@gmail.com>
2025-02-03 18:23:58 +0100

    fix: tf files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,19 @@ variable "ssh_public_key" {
   description = "SSH Public Key for VM access"
   type        = string
 }
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
