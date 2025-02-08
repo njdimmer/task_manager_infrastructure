44c58823a83ed4ef19d682977bc400853b728b80
Noah Dimmer <noahdimmer1@gmail.com>
2025-02-08 18:29:14 +0100

    fix: tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
       "sudo apt-get update",
       "sudo apt-get install -y docker-ce",
-      "sudo usermod -aG docker ${USER}",
+      "sudo usermod -aG docker azureuser",
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "docker --version",
