45ff5a72e67a40967e1bb5c43cb1c2d59dca4857
Noah Dimmer <noahdimmer1@gmail.com>
2025-02-08 18:17:51 +0100

    fix: tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,7 +109,28 @@ resource "azurerm_linux_virtual_machine" "vm" {
     public_key = var.ssh_public_key
   }
 
-  custom_data = filebase64("${path.module}/scripts/setup.sh")
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce",
+      "sudo usermod -aG docker ${USER}",
+      "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "docker --version",
+      "docker-compose --version"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "azureuser"
+      private_key = var.ssh_private_key
+      host        = azurerm_public_ip.public_ip.ip_address
+    }
+  }
 }
 
 resource "null_resource" "deploy" {
