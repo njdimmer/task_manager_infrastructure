dbb024360febd2503433e34ff540a5f9e2b9a832
Noah Dimmer <noahdimmer1@gmail.com>
2025-02-08 18:03:12 +0100

    fix: terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "task_manager"
+    storage_account_name  = "mystorageaccount"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 
